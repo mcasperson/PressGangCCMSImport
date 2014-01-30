ad77d5699351449ffa86b4a175bde662a9732404
Matthew Casperson <mcaspers@redhat.com>
2014-01-30 18:11:32 +1000

    Working on getting initial steps to be displayed

--- js/qnasteps.js
+++ js/qnasteps.js
@@ -43,62 +43,64 @@
      */
     var askForMainXML = new global.QNAStep(
         "Select the main XML file",
-        "Select the main XML file from the ZIP archive. Publican conventions mean the file should be named after the book title in Book_Info.xml.",
+        "Select the main XML file from the ZIP archive. Publican conventions mean the file should be named after the book title in Book_Info.xml. " +
+            "This import tool will attempt to read the Book_Info.xml file to find the book title, and from that select the main XML file. " +
+            "You only need to make a manual selection if the import tool could not find the main file, or if you want to override the default selection.",
         [
-            new global.QNAVariables(null, [
-                new global.QNAVariable(
-                    global.InputEnum.LISTBOX,
-                    null,
-                    "MainXMLFile",
-                    function (result, config, itemsCallback, valueCallback) {
+                new global.QNAVariables(null, [
+                    new global.QNAVariable(
+                        global.InputEnum.LISTBOX,
+                        null,
+                        "MainXMLFile",
+                        function (result, config, itemsCallback, valueCallback) {
 
-                        var findItems = function () {
-                            // here we get all the files from the zip and list any that might be relevant
-                            var retValue = [];
+                            var findItems = function () {
+                                // here we get all the files from the zip and list any that might be relevant
+                                var retValue = [];
 
+                                global.angular.forEach(config.ZipFileEntries, function (value, key) {
+                                    if (/^en-US\/.*?\.xml$/.test(value.filename)) {
+                                        retValue.push(value.filename);
+                                    }
+                                });
+
+                                itemsCallback(retValue);
+                            };
+
+                            // here we attempt to find a file called Book_Info.xml, look for the title element inside of it,
+                            // and use that to work out the main xml file name
+                            var foundBookInfo = false;
                             global.angular.forEach(config.ZipFileEntries, function (value, key) {
-                                if (/^en-US\/.*?\.xml$/.test(value.filename)) {
-                                    retValue.push(value.filename);
+                                if (/^en-US\/Book_Info\.xml$/.test(value.filename)) {
+
+                                    foundBookInfo = true;
+
+                                    new global.QNAZipModel().getTextFromFile(value, function (textFile) {
+                                        var match = /<title>(.*?)<\/title>/.exec(textFile);
+                                        if (match) {
+                                            var assumedMainXMLFile = "en-US/" + match[1].replace(/ /g, "_") + ".xml";
+
+                                            global.angular.forEach(config.ZipFileEntries, function (value, key) {
+                                                if (value.filename === assumedMainXMLFile) {
+                                                    valueCallback(assumedMainXMLFile);
+                                                    return false;
+                                                }
+                                            });
+                                        }
+
+                                        findItems();
+                                    });
+
+                                    return false;
                                 }
                             });
 
-                            itemsCallback(retValue);
-                        };
-
-                        // here we attempt to find a file called Book_Info.xml, look for the title element inside of it,
-                        // and use that to work out the main xml file name
-                        var foundBookInfo = false;
-                        global.angular.forEach(config.ZipFileEntries, function (value, key) {
-                            if (/^en-US\/Book_Info\.xml$/.test(value.filename)) {
-
-                                foundBookInfo = true;
-
-                                new global.QNAZipModel().getTextFromFile(value, function (textFile) {
-                                    var match = /<title>(.*?)<\/title>/.exec(textFile);
-                                    if (match) {
-                                        var assumedMainXMLFile = "en-US/" + match[1].replace(/ /g, "_") + ".xml";
-
-                                        global.angular.forEach(config.ZipFileEntries, function (value, key) {
-                                            if (value.filename === assumedMainXMLFile) {
-                                                valueCallback(assumedMainXMLFile);
-                                                return false;
-                                            }
-                                        });
-                                    }
-
-                                    findItems();
-                                });
-
-                                return false;
+                            if (!foundBookInfo) {
+                                findItems();
                             }
-                        });
-
-                        if (!foundBookInfo) {
-                            findItems();
                         }
-                    }
-                )
-            ])
+                    )
+                ])
         ],
         null,
         function (result, config, stepCallback, errorCallback) {
