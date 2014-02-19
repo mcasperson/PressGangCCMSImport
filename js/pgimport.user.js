b460d201ff5732e0c8dc5251c916f3b9b0b60326
Matthew Casperson <mcaspers@redhat.com>
2014-02-20 07:56:22 +1000

    Working on Mojo import

---
+++ js/pgimport.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        PressGang Import Tool
+// @namespace   https://www.jboss.org/pressgang
+// @description A collection of GreaseMonkey scripts for importing data from remote origins
+// @author      Matthew Casperson
+// @include     https://mojo.redhat.com/*
+// @require     http://code.jquery.com/jquery-2.0.3.min.js
+// @require     http://localhost:8000/js/mojoimport-0.0.1.js
+// @version     3.8.1
+// @grant       GM_xmlhttpRequest
+// @grant       unsafeWindow
+// @downloadURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
+// @downloadURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
+// @homepageURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/
+// ==/UserScript==
