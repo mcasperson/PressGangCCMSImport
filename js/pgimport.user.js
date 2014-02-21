b2bc913028981213f73363c991e184c348bbf545
Matthew Casperson <mcaspers@redhat.com>
2014-02-22 05:45:49 +1000

    Working on Mojo import

--- js/pgimport.user.js
+++ js/pgimport.user.js
@@ -6,7 +6,7 @@
 // @include     http://localhost:8000*
 // @require     http://code.jquery.com/jquery-2.0.3.min.js
 // @require     http://localhost:8000/js/mojoimport-0.0.22.js
-// @version     3.8.1
+// @version     0.0.1
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
 // @downloadURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
