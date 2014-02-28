99f43d9a2505bc74688bde4707674890c6ace150
Matthew Casperson <mcaspers@redhat.com>
2014-02-28 11:11:06 +1000

    Converted project to Maven WAR

--- src/main/webapp/js/pgimport.user.js
+++ src/main/webapp/js/pgimport.user.js
@@ -4,13 +4,13 @@
 // @description A collection of GreaseMonkey scripts for importing data from remote origins
 // @author      Matthew Casperson
 // @include     http://localhost:8000/*
-// @include     http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/*
+// @include     http://topicindex-dev.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/*
 // @require     http://code.jquery.com/jquery-2.0.3.min.js
-// @require     http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/mojoimport-0.0.30.js
+// @require     http://topicindex-dev.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/mojoimport-0.0.30.js
 // @version     0.0.1
 // @grant       GM_xmlhttpRequest
 // @grant       unsafeWindow
-// @downloadURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
-// @downloadURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
-// @homepageURL http://topika.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/
+// @downloadURL http://topicindex-dev.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
+// @downloadURL http://topicindex-dev.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/js/pgimport.user.js
+// @homepageURL http://topicindex-dev.ecs.eng.bne.redhat.com:8080/pressgang-ccms-import/
 // ==/UserScript==
