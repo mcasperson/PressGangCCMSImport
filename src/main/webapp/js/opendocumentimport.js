e927bfa2567c1fdac26028d4e374746ac997336b
Matthew Casperson <mcaspers@redhat.com>
2014-05-06 13:30:08 +1000

    Fixing https://bugzilla.redhat.com/show_bug.cgi?id=1093514

--- src/main/webapp/js/opendocumentimport.js
+++ src/main/webapp/js/opendocumentimport.js
@@ -761,7 +761,6 @@ define(
                                                     resultObject.contentSpec.push("Chapter: " + qnastart.escapeSpecTitle(title));
                                                     generalexternalimport.addTopicToSpec(topicGraph, content, title, resultObject.contentSpec.length - 1);
                                                 } else {
-                                                    padContentSpec(outlineLevel, parentLevel, resultObject.contentSpec);
                                                     var prefix = generalexternalimport.generateSpacing(outlineLevel);
                                                     resultObject.contentSpec.push(prefix + qnastart.escapeSpecTitle(title));
                                                     generalexternalimport.addTopicToSpec(topicGraph, content, title, resultObject.contentSpec.length - 1);
@@ -1515,23 +1514,6 @@ define(
                                         return content;
                                     };
 
-                                    var padContentSpec = function(currentLevel, previousLevel, contentSpec) {
-                                        /*
-                                         A content spec can not skip levels in the toc. So when we skip heading levels
-                                         (say a heading 3 under a heading 1) we need to pad the spec out.
-                                         */
-                                        if (currentLevel > previousLevel + 1) {
-                                            for (var missedSteps = previousLevel + 1; missedSteps < currentLevel; ++missedSteps) {
-                                                if (missedSteps === 1) {
-                                                    contentSpec.push("Chapter: Missing Chapter");
-                                                } else {
-                                                    var myPrefix = generalexternalimport.generateSpacing(missedSteps);
-                                                    contentSpec.push(myPrefix + "Section: Missing Section");
-                                                }
-                                            }
-                                        }
-                                    };
-
                                     var processHeader = function (content, contentNode, title, previousLevel, currentLevel, index, successCallback) {
 
                                         ++topicsAdded;
@@ -1554,16 +1536,11 @@ define(
                                         }
 
                                         /*
-                                         A content spec can not skip levels in the toc. So when we skip heading levels
-                                         (say a heading 3 under a heading 1) we need to pad the spec out.
+                                         We never jump more than one level
                                          */
-                                        padContentSpec(currentLevel, previousLevel, resultObject.contentSpec);
-
-                                        /*
-                                         Thanks to the loop above, levels never jump more than 1 place up.
-                                         */
-                                        previousLevel = currentLevel - 1;
-
+                                        if (newOutlineLevel + 1 > currentLevel) {
+                                            newOutlineLevel = currentLevel + 1;
+                                        }
 
                                         /*
                                          Some convenient statements about what is going on.
