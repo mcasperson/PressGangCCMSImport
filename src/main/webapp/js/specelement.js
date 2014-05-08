ac4744bf151bac9f49159b1651ef64d70895363c
Matthew Casperson <matthewcasperson@gmail.com>
2014-05-09 06:43:59 +1000

    Working on importing all RHEL 7 books

--- src/main/webapp/js/specelement.js
+++ src/main/webapp/js/specelement.js
@@ -469,7 +469,7 @@ define(['jquery', 'qna/qnautils', 'exports'], function(jquery, qnautils, exports
 
         if (!valid) {
             // because the supplied topic id was not in the list of ids for this topic
-            console.log(pgId + " was not in the list of existing topics. Options were " + this.pgIds.toString());
+            console.log(pgId + " was not in the list of existing topics. Options were " + this.pgIds.join());
             return ValidOrProcessed.INVALID;
         }
 
