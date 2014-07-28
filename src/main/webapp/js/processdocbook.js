aeff22aefc2cb020fff84fb843df9d8b994b2d9b
Matthew Casperson <matthewcasperson@gmail.com>
2014-07-28 11:20:34 +1000

    Added license info for LGPL v3

--- src/main/webapp/js/processdocbook.js
+++ src/main/webapp/js/processdocbook.js
@@ -1,4 +1,19 @@
 /*
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
     Takes DocBook XML that may be from an existing PressGang build or some other external source and
     processes it so it can be imported and used by PressGang. It also analyses the XML to generate
     the spec metadata.
