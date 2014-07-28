2efcbbb0dc3717c46500bcdfbb6484ab7ca5689e
Matthew Casperson <matthewcasperson@gmail.com>
2014-07-28 14:11:21 +1000

    Added license info for LGPL v3

--- src/main/webapp/js/reportsettings.js
+++ src/main/webapp/js/reportsettings.js
@@ -1,16 +1,20 @@
 /*
- This program is free software: you can redistribute it and/or modify
+ Copyright 2011-2014 Red Hat
+
+ This file is part of PresGang CCMS.
+
+ PresGang CCMS is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
+ PresGang CCMS is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
 
  You should have received a copy of the GNU Lesser General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ along with PresGang CCMS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
